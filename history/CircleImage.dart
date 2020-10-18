0983fd2245c52f150b6cb6504f8c5269d26637ff
zhangdezhi <1541609448@qq.com>
2020-10-18 20:56:02 +0800

    文件提交

--- history/CircleImage.dart
+++ history/CircleImage.dart
@@ -1,28 +1,13 @@
-import 'dart:ui';
-
+/**
+ * @author:dezhizhang
+ * @date:2020-10-18
+ * @desc:圆角图片
+*/
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
-
-// class MyApp extends StatelessWidget{
-//   @override
-//   Widget build(BuildContext context) {
-//     // TODO: implement build
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text('晓智商城'),
-//         ),
-//         body: HomeContent(),
-//       ),
-//       theme: ThemeData(
-//         primarySwatch: Colors.blueGrey
-//       ),
-//     );
-//   }
-// }
 
 class MyApp extends StatelessWidget{
   @override
@@ -53,7 +38,9 @@ class HomeContent extends StatelessWidget{
         decoration: BoxDecoration(
           color: Colors.pink,
           image: DecorationImage(
-            image:  NetworkImage('http://tugua.oss-cn-hangzhou.aliyuncs.com/16006151939994201.jpeg'),
+            image:  NetworkImage(
+              'http://tugua.oss-cn-hangzhou.aliyuncs.com/16006151939994201.jpeg'
+            ),
             fit: BoxFit.cover
           ),
           borderRadius: BorderRadius.circular(150)
