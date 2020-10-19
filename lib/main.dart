1f5000fc005a428b6b6d5646e6703fbf027f281d
zhangdezhi <1541609448@qq.com>
2020-10-19 23:01:12 +0800

    提交

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('晓智商城'),
+        ),
+        body: Text('hello'),
+      ),
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey
+      ),
+    );
+  }
+}
