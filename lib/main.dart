bc5930f627d3809b0b5b94ca4b66cfb1c0fc6ced
zhangdezhi <1541609448@qq.com>
2020-10-21 00:03:25 +0800

    卡片组件

--- lib/main.dart
+++ lib/main.dart
@@ -25,40 +25,72 @@ class MyApp extends StatelessWidget{
 }
 
 
+// class HomeContent extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//    return ListView(
+//      children: <Widget>[
+//        Card(
+//          margin: EdgeInsets.all(10),
+//          child: Column(
+//            children: <Widget>[
+//              AspectRatio(
+//                aspectRatio: 16/9,
+//                child:  Image.network(
+//                  'http://tugua.oss-cn-hangzhou.aliyuncs.com/16007371921474207.jpeg',
+//                  fit: BoxFit.cover,
+//                 ),
+//              ),
+//              ListTile(
+//               //  leading: ClipOval(
+//               //    child: Image.network(
+//               //      'http://tugua.oss-cn-hangzhou.aliyuncs.com/16007371921474207.jpeg',
+//               //      fit: BoxFit.cover,
+//               //     ),
+//               //  ),
+//               leading: CircleAvatar(
+//                 backgroundImage: NetworkImage('http://tugua.oss-cn-hangzhou.aliyuncs.com/16007371921474207.jpeg'),
+//               ),
+//                title: Text('晓智商城'),
+//                subtitle: Text('hello'),
+//              )
+//            ],
+//          ),
+//        )
+//      ],
+//    );
+//   }
+// }
+
 class HomeContent extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-   return ListView(
-     children: <Widget>[
-       Card(
-         margin: EdgeInsets.all(10),
-         child: Column(
-           children: <Widget>[
-             AspectRatio(
-               aspectRatio: 16/9,
-               child:  Image.network(
-                 'http://tugua.oss-cn-hangzhou.aliyuncs.com/16007371921474207.jpeg',
-                 fit: BoxFit.cover,
+    return ListView(
+      children: <Widget>[
+        Card(
+          margin: EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Image.network(
+                  'http://tugua.oss-cn-hangzhou.aliyuncs.com/16007371921474207.jpeg',
+                  fit: BoxFit.cover,
                 ),
-             ),
-             ListTile(
-              //  leading: ClipOval(
-              //    child: Image.network(
-              //      'http://tugua.oss-cn-hangzhou.aliyuncs.com/16007371921474207.jpeg',
-              //      fit: BoxFit.cover,
-              //     ),
-              //  ),
-              leading: CircleAvatar(
-                backgroundImage: NetworkImage('http://tugua.oss-cn-hangzhou.aliyuncs.com/16007371921474207.jpeg'),
               ),
-               title: Text('晓智商城'),
-               subtitle: Text('hello'),
-             )
-           ],
-         ),
-       )
-     ],
-   );
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage('http://tugua.oss-cn-hangzhou.aliyuncs.com/16007371921474207.jpeg'),
+                ),
+                title: Text('晓智商城'),
+                subtitle: Text('晓智商城页'),
+              )
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
