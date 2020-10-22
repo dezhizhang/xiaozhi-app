1ce64727a529054e45ffebac0b873726d2ec2d22
zhangdezhi <1541609448@qq.com>
2020-10-23 07:57:25 +0800

    提交

---
+++ lib/SingleChildScrollView.dart
@@ -0,0 +1,65 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('晓智商城'),
+        ),
+        body: HomeContent(),
+      ),
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey
+      ),
+    );
+  }
+}
+
+
+class HomeContent extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    String str = 'ABDCDEFGHIJKMK';
+    // TODO: implement build
+    return Scrollbar(
+      child: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            children: str.split('').map((e) => Text(e,textScaleFactor: 2.0)).toList()
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+// class HomeContent extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//    return Center(
+//      child: Text(
+//        '晓智商城',
+//        textDirection: TextDirection.ltr,
+//        style: TextStyle(
+//          fontSize: 40.0,
+//          color: Colors.pink
+//        ),
+//      ),
+//    );
+//   }
+// }
+
