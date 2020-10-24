12abba91f8f5e5fb7928a5f2d2b0736c4d18fb50
zhangdezhi <1541609448@qq.com>
2020-10-24 15:15:53 +0800

    路由参数

--- lib/main.dart
+++ lib/main.dart
@@ -2,49 +2,23 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-import './pages/form.dart';
-import './pages/search.dart';
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget{
-  final routes = {
-    '/form':(context) => FormPage(),
-    '/search':(context,{arguments}) => SearchPage(arguments:arguments),
-  };
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title:Text('晓知商城')
+          title: Text('晓智商城'),
         ),
         body: HomeContent(),
       ),
-      onGenerateRoute: (RouteSettings settings) {
-        final String name = settings.name;
-        final Function pageContentBuilder = this.routes[name];
-        if(pageContentBuilder != null) {
-          if(settings.arguments != null) {
-            final Route route = MaterialPageRoute(
-              builder: (context) => pageContentBuilder(context,
-              arguments:settings.arguments
-            )
-            );
-            return route;
-          } else {
-            final Route route = MaterialPageRoute(
-              builder: (context) => pageContentBuilder(context)
-            );
-            return route;
-          }
-        } 
-      },
       theme: ThemeData(
-        primarySwatch: Colors.pink
+        primarySwatch: Colors.blueGrey
       ),
     );
   }
@@ -54,27 +28,15 @@ class HomeContent extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center(
-      child: Column(
-        children: <Widget>[
-          RaisedButton(
-            child: Text('跳到表单'),
-            color: Colors.pink,
-            onPressed: () {
-              Navigator.pushNamed(context, '/form');
-            },
-          ),
-          RaisedButton(
-            child: Text('跳到搜索'),
-            color: Colors.yellow,
-            onPressed: () {
-              Navigator.pushNamed(context, '/search',arguments: {
-                "id":456
-              });
-            },
-          )
-        ],
-      ),
-    );
+   return Center(
+     child: Text(
+       '晓智商城',
+       textDirection: TextDirection.ltr,
+       style: TextStyle(
+         fontSize: 40.0,
+         color: Colors.pink
+       ),
+     ),
+   );
   }
 }
