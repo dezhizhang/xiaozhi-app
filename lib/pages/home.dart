10ec7bb2dab232540ff6dfa2703a0d8605c621d7
zhangdezhi <1541609448@qq.com>
2020-10-25 16:25:26 +0800

    连接

---
+++ lib/pages/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatefulWidget{
+  _Home createState() => _Home();
+}
+
+class _Home extends State<Home>{
+  @override
+  Widget build(BuildContext context) {
+    return Text('home');
+  }
+}
