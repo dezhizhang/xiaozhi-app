3b3f33ab7ce8f7a9b5fc6aa45ed76095a5afee7a
zhangdezhi <1541609448@qq.com>
2020-10-25 15:56:36 +0800

    完善首页

--- lib/main.dart
+++ lib/main.dart
@@ -5,84 +5,27 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
-  
+class MyApp extends StatefulWidget{
+  MyApp({Key key}):super(key:key);
+  _MyApp createState() => _MyApp();
+}
+
+class _MyApp extends State<MyApp>{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('晓智商城'),
+          title: Text('贵彩办公'),
         ),
-        body: HomeContent(),
+        body: Text('hello'),
       ),
-      routes: {
-      //  '/date':(context) => DatePage(),
-      //  '/swiper':(context) => SwierPage(),
-      //  '/dialog':(context) => DialogPage(),
-      //  '/http':(context) => HttpPage(),
-      //  '/diao':(context) => DiaoPage(),
-      //  '/news':(context) => NewsPage(),
-      },
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey
+        primarySwatch: Colors.pink
       ),
     );
   }
 }
-
-
-class HomeContent extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: <Widget>[
-          RaisedButton(
-            child: Text('单行文本'),
-            onPressed: () {
-              Navigator.pushNamed(context, '/date');
-            },
-          ),
-          RaisedButton(
-            child: Text('轮播图'),
-            onPressed: () {
-              Navigator.pushNamed(context, '/swiper');
-            },
-          ),
-          RaisedButton(
-            child: Text('dialog'),
-            onPressed: () {
-              Navigator.pushNamed(context, '/dialog');
-            },
-          ),
-          RaisedButton(
-            child: Text('network'),
-            onPressed: () {
-              Navigator.pushNamed(context, '/http');
-            },
-          ),
-          RaisedButton(
-            child: Text('diao'),
-            onPressed: () {
-              Navigator.pushNamed(context, '/diao');
-            },
-          ),
-          RaisedButton(
-            child: Text('news'),
-            onPressed: () {
-              Navigator.pushNamed(context, '/news');
-            },
-          ),
-         
-        ],
-      )
-    );
-  }
-}
-
-
 
 
