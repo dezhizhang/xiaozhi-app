9f8c75946c9329960e19b3befc3fd62739de225f
zhangdezhi <1541609448@qq.com>
2020-10-26 22:54:17 +0800

    代码补全

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,32 @@
-import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import './routers/router.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget{
+  MyApp({Key key}):super(key:key);
+  _MyApp createState() => _MyApp();
+}
+
+
+class _MyApp extends State<MyApp>{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('晓智商城'),
-        ),
-        body: HomeContent(),
-      ),
+      onGenerateRoute: onGenerateRoute,
+      initialRoute: '/',
       theme: ThemeData(
-        primarySwatch: Colors.blue
+        primarySwatch: Colors.pink
       ),
     );
   }
 }
 
 
-class HomeContent extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Column(
-      children: <Widget>[
-        NavBar(color: Colors.blue, title: "标题"), 
-        //背景为白色，则title自动为黑色
-        NavBar(color: Colors.white, title: "标题"),
-      ],
-    );
-  }
-}
 
 
-class NavBar extends StatelessWidget {
-  final String title;
-  final Color color; //背景颜色
 
-  NavBar({
-    Key key,
-    this.color,
-    this.title,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      constraints: BoxConstraints(
-        minHeight: 52,
-        minWidth: double.infinity,
-      ),
-      decoration: BoxDecoration(
-        color: color,
-        boxShadow: [
-          //阴影
-          BoxShadow(
-            color: Colors.black26,
-            offset: Offset(0, 3),
-            blurRadius: 3,
-          ),
-        ],
-      ),
-      child: Text(
-        title,
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          //根据背景色亮度来确定Title颜色
-          color: color.computeLuminance() < 0.5 ? Colors.white : Colors.black,
-        ),
-      ),
-      alignment: Alignment.center,
-    );
-  }
-}
---
+++ testinfo/main.dart
@@ -0,0 +1,83 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('晓智商城'),
+        ),
+        body: HomeContent(),
+      ),
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+    );
+  }
+}
+
+
+class HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: <Widget>[
+        NavBar(color: Colors.blue, title: "标题"), 
+        //背景为白色，则title自动为黑色
+        NavBar(color: Colors.white, title: "标题"),
+      ],
+    );
+  }
+}
+
+
+class NavBar extends StatelessWidget {
+  final String title;
+  final Color color; //背景颜色
+
+  NavBar({
+    Key key,
+    this.color,
+    this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(
+        minHeight: 52,
+        minWidth: double.infinity,
+      ),
+      decoration: BoxDecoration(
+        color: color,
+        boxShadow: [
+          //阴影
+          BoxShadow(
+            color: Colors.black26,
+            offset: Offset(0, 3),
+            blurRadius: 3,
+          ),
+        ],
+      ),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          //根据背景色亮度来确定Title颜色
+          color: color.computeLuminance() < 0.5 ? Colors.white : Colors.black,
+        ),
+      ),
+      alignment: Alignment.center,
+    );
+  }
+}
