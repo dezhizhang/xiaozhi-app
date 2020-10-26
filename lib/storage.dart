71dc6a399b55a40baa50de5d5f125992db5be2a4
zhangdezhi <1541609448@qq.com>
2020-10-27 07:12:00 +0800

    数据的保存与获取

--- lib/storage.dart
+++ lib/storage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './utils/storage.dart';
 
 class StoragePage extends StatelessWidget{
   @override
@@ -24,6 +25,26 @@ class _HomeContent extends State<HomeContent> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Text('hello');
+    return Center(
+      child: Column(
+        children: <Widget>[
+          RaisedButton(
+            child: Text('保存数据'),
+            color: Colors.pink,
+            onPressed: () async{
+              await Storage.setString('username', 'hello');
+            },
+          ),
+          RaisedButton(
+            child: Text('获取数据'),
+            color: Colors.yellow,
+            onPressed: () async{
+              var username = await Storage.getString('username');
+              print(username);
+            },
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/utils/storage.dart
@@ -0,0 +1,13 @@
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Storage{
+  static Future<void> setString(key,value) async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    sp.setString(key, value);
+  }
+  static Future<String> getString(key) async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    return sp.getString(key);
+  }
+}
