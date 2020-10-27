c1f73701c9cbcb7c954afac173f0409b763749ea
zhangdezhi <1541609448@qq.com>
2020-10-28 07:38:30 +0800

    提交

---
+++ app/main.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/material.dart';
+import './routers/router.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget{
+  MyApp({Key key}):super(key:key);
+  _MyApp createState() => _MyApp();
+}
+
+
+class _MyApp extends State<MyApp>{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: onGenerateRoute,
+      initialRoute: '/',
+      theme: ThemeData(
+        primarySwatch: Colors.pink
+      ),
+    );
+  }
+}
+
+
+
+
+
