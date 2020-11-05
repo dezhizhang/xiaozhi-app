3aedb7b11ef98e270a986db4862e04bbb382a985
zhangdezhi <1541609448@qq.com>
2020-11-05 23:37:10 +0800

    buildSuggestions

--- lib/search.dart
+++ lib/search.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import './asset.dart';
 
@@ -17,11 +19,75 @@ class _SearchPage extends State<SearchPage>{
           IconButton(
             icon: Icon(Icons.search),
             onPressed: () {
-              print('search');
+              showSearch(context: null, delegate: seachBarDelegate());
             },
           ),
         ],
       ),
+    );
+  }
+}
+
+class seachBarDelegate extends SearchDelegate<String>{
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // TODO: implement buildActions
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () => query = "",
+      )
+    ];
+  }
+  @override
+  Widget buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.arrow_menu,
+        progress: transitionAnimation,
+      ),
+      onPressed: ()=>close(context,null),
+    );
+  }
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    return Container(
+      width: 100,
+      height: 100,
+      child: Card(
+        color: Colors.redAccent,
+        child: Center(
+          child: Text(query),
+        ),
+      ),
+    );
+  }
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestionList = query.isEmpty ? recent:searchList.where((input) => input.startsWith(query)).toList();
+
+    // TODO: implement buildSuggestions
+    return ListView.builder(
+      itemCount: suggestionList.length,
+      itemBuilder: (context,index)=> ListTile(
+        title: RichText(
+          text: TextSpan(
+            text: suggestionList[index].substring(0,query.length),
+            style: TextStyle(
+              color: Colors.pink,
+              fontWeight: FontWeight.bold
+            ),
+            children: [
+              TextSpan(
+                text: suggestionList[index].substring(query.length),
+                style: TextStyle(color: Colors.grey)
+              )
+            ]
+          ),
+        ),
+      )
     );
   }
 }
