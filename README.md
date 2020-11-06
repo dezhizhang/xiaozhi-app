540c9b316b43bae3f30de393815092b7611eb2bb
zhangdezhi <1541609448@qq.com>
2020-11-06 23:56:44 +0800

    提交

--- README.md
+++ README.md
@@ -2103,6 +2103,86 @@ class _ToolTipPage extends State<ToolTipPage> {
   }
 }
 ```
+### weap布局
+```
+
+import 'package:flutter/material.dart';
+
+
+
+
+class WrapPage extends StatefulWidget{
+  _WrapPage createState() => _WrapPage();
+}
+
+class _WrapPage extends State<WrapPage> {
+  @override
+  List<Widget> list;
+  @override
+  void initState() { 
+    super.initState();
+    list = List<Widget>()..add(buildAddButton());
+  }
+  Widget buildAddButton() {
+     return GestureDetector(
+       onTap: () {
+         if(list.length <9) {
+            setState(() {
+                list.insert(list.length, buildPhoto());
+            });
+         }
+       },
+       child: Padding(
+         padding: EdgeInsets.all(8),
+         child: Container(
+           width: 80,
+           height: 80,
+           color: Colors.grey,
+           child: Icon(Icons.add),
+         ),
+       ),
+     );
+  }
+
+  Widget buildPhoto() {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: Container(
+        width: 80,
+        height: 80,
+        color: Colors.pink,
+        child: Center(
+          child: Text('hello'),
+        )
+      ),
+    );
+  }
+  
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('wrap'),
+      ),
+      body: Center(
+        child: Opacity(
+          opacity: 0.8,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 2,
+            color: Colors.green,
+            child: Wrap(
+              children: list,
+              spacing: 26.0,
+            ),
+          )
+        ),
+      ),
+    );
+  }
+}
+
+```
+
 
 
 
--- lib/wrap.dart
+++ lib/wrap.dart
@@ -2,30 +2,74 @@
 import 'package:flutter/material.dart';
 
 
+
+
 class WrapPage extends StatefulWidget{
   _WrapPage createState() => _WrapPage();
 }
 
-class _WrapPage extends State<WrapPage>{
+class _WrapPage extends State<WrapPage> {
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('wrapper'),
+  List<Widget> list;
+  @override
+  void initState() { 
+    super.initState();
+    list = List<Widget>()..add(buildAddButton());
+  }
+  Widget buildAddButton() {
+     return GestureDetector(
+       onTap: () {
+         if(list.length <9) {
+            setState(() {
+                list.insert(list.length, buildPhoto());
+            });
+         }
+       },
+       child: Padding(
+         padding: EdgeInsets.all(8),
+         child: Container(
+           width: 80,
+           height: 80,
+           color: Colors.grey,
+           child: Icon(Icons.add),
+         ),
+       ),
+     );
+  }
+
+  Widget buildPhoto() {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: Container(
+        width: 80,
+        height: 80,
+        color: Colors.pink,
+        child: Center(
+          child: Text('hello'),
+        )
       ),
-      body: HomeContent(),
     );
   }
-}
-
-class HomeContent extends StatefulWidget{
-  _HomeContent createState() => _HomeContent();
-}
-
-class _HomeContent extends State<HomeContent> {
-  @override
+  
   Widget build(BuildContext context) {
-    return Text('hello');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('wrap'),
+      ),
+      body: Center(
+        child: Opacity(
+          opacity: 0.8,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 2,
+            color: Colors.green,
+            child: Wrap(
+              children: list,
+              spacing: 26.0,
+            ),
+          )
+        ),
+      ),
+    );
   }
 }
