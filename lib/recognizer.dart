32a49329469cf2c1a7e3bc5f9a14c63239bab5f1
zhangdezhi <1541609448@qq.com>
2020-11-07 19:25:24 +0800

    提交

--- lib/recognizer.dart
+++ lib/recognizer.dart
@@ -7,48 +7,6 @@ class RecognizerPage extends StatefulWidget{
   _RecognizerPage createState() => _RecognizerPage();
 }
 
-// class _RecognizerPage extends State<RecognizerPage>{
-//   TapGestureRecognizer _tapGestureRecognizer = new TapGestureRecognizer();
-//   bool _toggle = false;
-//   @override
-//   Widget build(BuildContext context) {
-//     // TODO: implement build
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('more event'),
-//       ),
-//       body: Center(
-//         child: Text.rich(
-//           TextSpan(
-//             children:[
-//               TextSpan(text: '你好世界'),
-//               TextSpan(
-//                 text: '点我变色',
-//                 style: TextStyle(
-//                   fontSize: 30,
-//                   color: _toggle ? Colors.red:Colors.blue
-//                 ),
-//                 recognizer: _tapGestureRecognizer
-//                 ..onTap = () {
-//                   setState(() {
-//                     _toggle = !_toggle;
-//                   });
-//                 }
-//               ),
-//               TextSpan(text: '你好世界')
-//             ] 
-//           )
-//         ),
-//       )
-//     );
-//   }
-// }
-
-
-
-
-
-
 class _RecognizerPage extends State<RecognizerPage>{
   TapGestureRecognizer _tapGestureRecognizer = TapGestureRecognizer();
   bool _toggle = false;
