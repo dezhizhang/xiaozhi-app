c4f9d182c5849b82e015dc9bb1e8233b93f5887f
zhangdezhi <1541609448@qq.com>
2020-11-07 19:02:34 +0800

    提交

--- lib/recognizer.dart
+++ lib/recognizer.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
  
@@ -5,8 +7,50 @@ class RecognizerPage extends StatefulWidget{
   _RecognizerPage createState() => _RecognizerPage();
 }
 
+// class _RecognizerPage extends State<RecognizerPage>{
+//   TapGestureRecognizer _tapGestureRecognizer = new TapGestureRecognizer();
+//   bool _toggle = false;
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('more event'),
+//       ),
+//       body: Center(
+//         child: Text.rich(
+//           TextSpan(
+//             children:[
+//               TextSpan(text: '你好世界'),
+//               TextSpan(
+//                 text: '点我变色',
+//                 style: TextStyle(
+//                   fontSize: 30,
+//                   color: _toggle ? Colors.red:Colors.blue
+//                 ),
+//                 recognizer: _tapGestureRecognizer
+//                 ..onTap = () {
+//                   setState(() {
+//                     _toggle = !_toggle;
+//                   });
+//                 }
+//               ),
+//               TextSpan(text: '你好世界')
+//             ] 
+//           )
+//         ),
+//       )
+//     );
+//   }
+// }
+
+
+
+
+
+
 class _RecognizerPage extends State<RecognizerPage>{
-  TapGestureRecognizer _tapGestureRecognizer = new TapGestureRecognizer();
+  TapGestureRecognizer _tapGestureRecognizer = TapGestureRecognizer();
   bool _toggle = false;
   @override
   Widget build(BuildContext context) {
@@ -18,7 +62,7 @@ class _RecognizerPage extends State<RecognizerPage>{
       body: Center(
         child: Text.rich(
           TextSpan(
-            children:[
+            children: [
               TextSpan(text: '你好世界'),
               TextSpan(
                 text: '点我变色',
@@ -27,17 +71,18 @@ class _RecognizerPage extends State<RecognizerPage>{
                   color: _toggle ? Colors.red:Colors.blue
                 ),
                 recognizer: _tapGestureRecognizer
-                ..onTap = () {
+                ..onTap = (){
                   setState(() {
                     _toggle = !_toggle;
                   });
                 }
               ),
               TextSpan(text: '你好世界')
-            ] 
+            ]
           )
         ),
-      )
+      ),
     );
   }
 }
+
