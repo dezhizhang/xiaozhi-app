f04b4b574bebf648d88efc1580dafec3193fb499
zhangdezhi <1541609448@qq.com>
2020-11-07 11:45:34 +0800

    修改文件

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,32 @@
 
 import 'package:flutter/material.dart';
+import './routers/router.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget{
+  MyApp({Key key}):super(key:key);
+  _MyApp createState() => _MyApp();
+}
+
+
+class _MyApp extends State<MyApp>{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('晓智商城'),
-        ),
-        body: Text('hello'),
-      ),
-     
+      onGenerateRoute: onGenerateRoute,
+      initialRoute: '/',
       theme: ThemeData(
-        primarySwatch: Colors.blue
+        primarySwatch: Colors.pink
       ),
     );
   }
 }
+
+
 
 
 
