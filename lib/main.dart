b6451a41c812fdde23bb922cdb82362a36ef9c4f
zhangdezhi <1541609448@qq.com>
2020-11-07 16:40:08 +0800

    _HomeContent

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,52 @@
-
 import 'package:flutter/material.dart';
-import './routers/router.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget{
-  MyApp({Key key}):super(key:key);
-  _MyApp createState() => _MyApp();
-}
-
-
-class _MyApp extends State<MyApp>{
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: onGenerateRoute,
-      initialRoute: '/',
-      theme: ThemeData(
-        primarySwatch: Colors.pink
+      // title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: HomeContent(),
       ),
     );
   }
 }
 
+class  HomeContent extends StatefulWidget{
+  _HomeContent createState() => _HomeContent();
+}
 
-
-
-
+class _HomeContent extends State<HomeContent>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Stack(
+      children: <Widget>[
+        Listener(
+          child: ConstrainedBox(
+            constraints: BoxConstraints.tight(Size(300,200)),
+            child: DecoratedBox(
+              decoration: BoxDecoration(color: Colors.blue),
+            )
+          ),
+          onPointerDown: (event) => print('donw0'),
+        ),
+        Listener(
+          child: ConstrainedBox(
+            constraints: BoxConstraints.tight(Size(200,100)),
+            child: Center(
+              child: Text('左上解200*100范围被点透'),
+            ),
+          ),
+          onPointerDown: (event) => print('down1'),
+          behavior: HitTestBehavior.translucent,
+        )
+      ],
+    );
+  }
+}
