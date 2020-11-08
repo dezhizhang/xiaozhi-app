68ff3d1b4403d2e184b2dd41d4b3e243a832c3c5
zhangdezhi <1541609448@qq.com>
2020-11-08 15:38:18 +0800

    更新数据完成

--- lib/services/service.dart
+++ lib/services/service.dart
@@ -1,9 +1,10 @@
 
 
 import 'package:dio/dio.dart';
+import '../utils/common.dart';
 
 class Service {
   getFocuInfo() async{
-    return  await Dio().get('https://www.guicaioa.com/api/focus/info');
+    return  await Dio().get("$URL/focus/info");
   }
 }
---
+++ lib/utils/common.dart
@@ -0,0 +1,1 @@
+const URL = 'https://www.guicaioa.com/api';
