5dbfcfe1bc5647222671c965a02c03a02869634c
zhangdezhi <1541609448@qq.com>
2020-11-09 23:31:52 +0800

    拼接数据完

--- lib/pages/category/category.dart
+++ lib/pages/category/category.dart
@@ -9,7 +9,8 @@ class Category extends StatefulWidget{
 }
 
 class _Category extends State<Category>{
-    List<CategoryModelItem> list = [];
+  int   _selectIndex = 0;
+  List<CategoryModelItem> list = [];
   @override
   void initState() {
     // TODO: implement initState
@@ -28,6 +29,7 @@ class _Category extends State<Category>{
   }
   @override
   Widget build(BuildContext context) {
+   
     ScreenAdapter.init(context);
     return Row(
       children: <Widget>[
@@ -42,12 +44,15 @@ class _Category extends State<Category>{
                 children: <Widget>[
                   InkWell(
                     onTap: () {
-
+                      setState(() {
+                        _selectIndex = index;
+                      });
                     },
                     child: Container(
                       alignment: Alignment.center,
                       width: double.infinity,
                       height: ScreenAdapter.height(56),
+                      color: _selectIndex == index ? Colors.red:Colors.white,
                       child: Text(
                         this.list[index].title,
                         textAlign: TextAlign.center,
@@ -76,28 +81,22 @@ class _Category extends State<Category>{
                   crossAxisSpacing: ScreenAdapter.width(10),
                   mainAxisSpacing: ScreenAdapter.width(10),
                 ),
-                itemCount: 10,
+                itemCount: list[this._selectIndex].items.length,
                 itemBuilder: (context,index) {
                   return Container(
-                    // decoration: BoxDecoration(
-                    //   border:Border.all(
-                    //     color:Color.fromRGBO(240, 246, 246, 0.9),
-                    //     width: ScreenAdapter.width(1)
-                    //   )
-                    // ),
                     color: Colors.white,
                     child: Column(
                       children: <Widget>[
                         AspectRatio(
                           aspectRatio: 1 / 1,
                           child: Image.network(
-                            'http://tugua.oss-cn-hangzhou.aliyuncs.com/1600737225621499.jpeg',
+                            list[this._selectIndex].items[index].url,
                             fit: BoxFit.cover,
                           ),
                         ),
                         Container(
                           height: ScreenAdapter.height(28),
-                          child: Text('女装'),
+                          child: Text('${list[this._selectIndex].items[index].title}'),
                         )
                       ],
                     ),
