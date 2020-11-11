6e5dfa1b1d27eb45dc0595feba361ca8ddac749a
zhangdezhi <1541609448@qq.com>
2020-11-12 00:40:21 +0800

    完善功能

--- lib/pages/product/product.dart
+++ lib/pages/product/product.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
+import 'package:flutter/material.dart';
+import '../../utils/utils.dart';
+import './productItem.dart';
 class Product extends StatefulWidget{
   Map arguments;
   Product({Key key,this.arguments}):super(key:key);
@@ -9,11 +12,20 @@ class Product extends StatefulWidget{
 class _Product extends State<Product>{
   @override
   Widget build(BuildContext context) {
+    ScreenAdapter.init(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('商品列表'),
       ),
-      body: Text('商品列表'),
+      body: Padding(
+        padding: EdgeInsets.all(ScreenAdapter.width(10)),
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context,index) {
+            return ProductItem();
+          }
+        ),
+      ),
     );
   }
 }
