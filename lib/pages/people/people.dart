a2877986335ce156b89cdb5d23b660f947ae1b45
zhangdezhi <1541609448@qq.com>
2020-12-23 23:22:00 +0800

    封装组件

--- lib/pages/people/people.dart
+++ lib/pages/people/people.dart
@@ -22,61 +22,101 @@ class _People extends State<People>{
   Widget build(BuildContext context) {
     ScreenAdapter.init(context);
     return Container(
+      width: ScreenAdapter.width(750),
+      height: ScreenAdapter.height(1334),
       color: Color.fromRGBO(238,238,238,1),
       child: Stack(
         children: <Widget>[
-          // Container(
-          //   color: Colors.white,
-          //   width: ScreenAdapter.height(750),
-          //   height: ScreenAdapter.width(489),
-          //   child:Container(
-          //   width: ScreenAdapter.width(750),
-          //   height: ScreenAdapter.height(322),
-          //   decoration: BoxDecoration(
-          //     image: DecorationImage(
-          //       fit: BoxFit.cover,
-          //       image: NetworkImage('http://tugua.oss-cn-hangzhou.aliyuncs.com/16086451694422710.png')
-          //     ),
-          //   ),
-          //   child: Text('HELLO'),
-          // ),
-          // ),
-          
-          // Container(
-          //   margin: EdgeInsets.only(top:ScreenAdapter.height(322)),
-          //   color: Colors.white,
-          //   width: ScreenAdapter.width(750),
-          //   height: ScreenAdapter.height(1012),
-          //   child: ListView(
-          //     children: <Widget>[
-          //       Container(
-          //         color: Colors.yellow,
-          //         width: ScreenAdapter.width(750),
-          //         height: ScreenAdapter.height(120),
-          //       )
-          //     ],
-          //   ),
-          // ),
-          // ListView(
-          //   children: <Widget>[
-          //     Container(
-          //       width: ScreenAdapter.width(750),
-          //       height: ScreenAdapter.height(120),
-          //       margin: EdgeInsets.only(top:ScreenAdapter.height(322),bottom: ),
-          //       decoration: BoxDecoration(
-          //         color: Colors.white,
-          //         border: Border(
-          //           bottom: BorderSide(
-          //             width: 1,
-          //             color: Color.fromRGBO(179,39,79,1)
-          //           )
-          //         )
-          //       ),
-          //       child: Row(),
-          //     ),
-
-          //   ],
-          // ),
+          Container(
+            width: ScreenAdapter.width(750),
+            height: ScreenAdapter.height(322),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage('http://tugua.oss-cn-hangzhou.aliyuncs.com/16086451694422710.png')
+              ),
+            ),
+            child: Text('HELLO'),
+          ),
+          Container(
+            margin: EdgeInsets.only(top:ScreenAdapter.height(322)),
+            // color: Colors.white,
+            width: ScreenAdapter.width(750),
+            height: ScreenAdapter.height(1012),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(bottom:ScreenAdapter.height(10)),
+                  color: Colors.white,
+                  width: ScreenAdapter.width(750),
+                  height: ScreenAdapter.height(205),
+                  child: Text('hello'),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left:ScreenAdapter.width(20),right: ScreenAdapter.width(20)),
+                  width: ScreenAdapter.width(750),
+                  height: ScreenAdapter.height(100),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border(
+                      bottom: BorderSide(
+                        width: 1,
+                        color: Color.fromRGBO(246,246,250,1)
+                      )
+                    )
+                  ),
+                  child: Flex(
+                        direction: Axis.horizontal,
+                        children: <Widget>[
+                          Expanded(
+                            flex: 1,
+                            child:Icon(Icons.star_outline),
+                          ),
+                          Expanded(
+                            flex: 4,
+                            child: Text('我的收藏'),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Text('hello'),
+                          ),
+                        ],
+                    ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left:ScreenAdapter.width(20),right: ScreenAdapter.width(20)),
+                  width: ScreenAdapter.width(750),
+                  height: ScreenAdapter.height(100),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border(
+                      bottom: BorderSide(
+                        width: 1,
+                        color: Color.fromRGBO(246,246,250,1)
+                      )
+                    )
+                  ),
+                  child: Flex(
+                        direction: Axis.horizontal,
+                        children: <Widget>[
+                          Expanded(
+                            flex: 1,
+                            child:Icon(Icons.star_outline),
+                          ),
+                          Expanded(
+                            flex: 4,
+                            child: Text('我的收藏'),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Text('hello'),
+                          ),
+                        ],
+                    ),
+                )
+              ],
+            ),
+          ),
           Positioned(
             top: 120,
             left: 20,
@@ -94,20 +134,6 @@ class _People extends State<People>{
         ],
       ),
     );
-    // return Stack(
-    //   children: <Widget>[
-    //     Container(
-    //       child: Text('header'),
-    //     ),
-    //     Positioned(
-    //       top: 240,
-    //       left: 40,
-    //       right: 40,
-    //       child: Text('hello'),
-
-    //     )
-    //   ],
-    // );
   }
 }
 
