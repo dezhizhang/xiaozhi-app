b1f4a2dfe2b8e06251834b54173fe806c2de3501
zhangdezhi <1541609448@qq.com>
2021-01-02 15:10:40 +0800

    修改文件

--- lib/pages/people/people.dart
+++ lib/pages/people/people.dart
@@ -21,65 +21,9 @@ class People extends StatefulWidget{
 class _People extends State<People>{
   @override
   Widget build(BuildContext context) {
-    ScreenAdapter.init(context);
-    return Container(
-      width: ScreenAdapter.width(750),
-      height: ScreenAdapter.height(1334),
-      color: Color.fromRGBO(238,238,238,1),
-      child: Stack(
-        children: <Widget>[
-          Container(
-            width: ScreenAdapter.width(750),
-            height: ScreenAdapter.height(322),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: NetworkImage('http://tugua.oss-cn-hangzhou.aliyuncs.com/16086451694422710.png')
-              ),
-            ),
-            child: Text('HELLO'),
-          ),
-          Container(
-            margin: EdgeInsets.only(top:ScreenAdapter.height(322)),
-            // color: Colors.white,
-            width: ScreenAdapter.width(750),
-            height: ScreenAdapter.height(1012),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(bottom:ScreenAdapter.height(10)),
-                  color: Colors.white,
-                  width: ScreenAdapter.width(750),
-                  height: ScreenAdapter.height(205),
-                  child: Text('hello'),
-                ),
-                PeopleItem(margin:0,title: '个人资料',routeName: "/information"),
-                PeopleItem(margin:0,title: '我的收藏',routeName: "/collection",),
-                PeopleItem(margin:0,title: '我的卡券',routeName: "/coupon",),
-                PeopleItem(margin: 10.0,title: '我的金币',routeName: '/money'),
-                PeopleItem(margin:0,title: '课程回放'),
-              ],
-            ),
-          ),
-          Positioned(
-            top: 120,
-            left: 20,
-            right: 20,
-            child: Container(
-              width: ScreenAdapter.width(670),
-              height: ScreenAdapter.height(302),
-              child: Text('hello'),
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(238, 238, 238,1),
-                borderRadius: BorderRadius.circular(20)
-              ),
-            ),
-          )
-        ],
-      ),
-    );
+    // TODO: implement build
+    return Text('hello');
   }
 }
-
 
 
