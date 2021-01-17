94b8210236545c53807346e1c692eb94adeff283
zhangdezhi <1541609448@qq.com>
2021-01-17 16:15:48 +0800

    添加分类模块

--- lib/pages/category/categoryItem.dart
+++ lib/pages/category/categoryItem.dart
@@ -29,13 +29,55 @@ class _CategoryItem extends State<CategoryItem>{
       child: Row(
         children: <Widget>[
           Container(
-            color: Colors.red,
-            width:ScreenAdapter.width(140),
+            width:ScreenAdapter.width(160),
             height:ScreenAdapter.height(140),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage('http://tugua.oss-cn-hangzhou.aliyuncs.com/16006152126699263.jpeg')
+              )
+            ),
+            // child: Image.network('http://tugua.oss-cn-hangzhou.aliyuncs.com/16006152126699263.jpeg',fit: BoxFit.cover),
           ),
           Container(
+            alignment: Alignment.centerLeft,
             margin: EdgeInsets.only(left:ScreenAdapter.width(20)),
-            child: Text('hello'),
+            child: Column(
+             children: <Widget>[
+               Container(
+                 alignment: Alignment.centerLeft,
+                 margin: EdgeInsets.only(top:ScreenAdapter.height(20),bottom: ScreenAdapter.height(30)),
+                 child:Text('品牌男士休闲运动装'),
+               ),
+               Container(
+                 child: Row(
+                   children: <Widget>[
+                      Text('￥280.00'),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(16,142,239,1),
+                          borderRadius: BorderRadius.circular(20)
+                        ),
+                        margin: EdgeInsets.only(left:ScreenAdapter.width(82)),
+                        width:ScreenAdapter.width(130),
+                        height:ScreenAdapter.height(40),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.card_travel,color: Colors.white),
+                            Text('购买',style: TextStyle(
+                              color: Colors.white,
+                              fontSize: ScreenAdapter.fontSize(24)
+                            ))
+                          ],
+                        ),
+                      )
+                   ],
+                 ),
+               )
+             ],
+            )
           )
         ],
       ),
