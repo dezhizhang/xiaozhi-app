e47f0a8d2c2b1512d84719318d8a3ccd71ff5c84
dezhizhang <1541609448@qq.com>
2021-02-14 11:07:03 +0800

    添加应用

--- lib/pages/search/clearBtn.dart
+++ lib/pages/search/clearBtn.dart
@@ -8,17 +8,20 @@ class ClearBtn extends StatefulWidget {
 class _ClearBtn extends State<ClearBtn> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Container(
-        width: ScreenAdapter.width(750),
-        height: ScreenAdapter.height(60),
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-            border: Border.all(
-          width: 1,
-          color: Colors.red,
-        )),
-        child: Text("删除搜索历史")
-      );
+      width: ScreenAdapter.width(750),
+      height: ScreenAdapter.height(60),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        border: Border.all(
+          width:1,
+          color:Color.fromRGBO(204,204,204,1)
+        )
+      ),
+      child: Text('删除搜索历史',style: TextStyle(
+        fontSize: ScreenAdapter.fontSize(32),
+        fontFamily: '微软雅黑'
+      )),
+    );
   }
 }
