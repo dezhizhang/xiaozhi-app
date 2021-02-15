5dc964322355e875d9358a5670bef2f00598ddea
dezhizhang <1541609448@qq.com>
2021-02-15 21:17:23 +0800

    增加缓存

--- lib/utils/search.dart
+++ lib/utils/search.dart
@@ -5,7 +5,13 @@ import './storage.dart';
 
 class Search {
   static setSearch(value) async{
-    var searchList = json.decode(await Storage.getString("searchList"));
-
+    var searchList = [];
+    try {
+      searchList = json.decode(await Storage.getString("searchList"));
+    } catch(e) {
+      List tempList = new List();
+      tempList.add(value);
+      await Storage.setString('searchList', json.encode(tempList));
+    }
   }
 }
