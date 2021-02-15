ddd2118225a5443b2ba4614d18f17c9823c8296c
dezhizhang <1541609448@qq.com>
2021-02-15 21:28:45 +0800

    提交

--- lib/utils/search.dart
+++ lib/utils/search.dart
@@ -5,9 +5,12 @@ import './storage.dart';
 
 class Search {
   static setSearch(value) async{
-    var searchList = [];
     try {
-      searchList = json.decode(await Storage.getString("searchList"));
+      List searchList = json.decode(await Storage.getString("searchList"));
+      bool hasSearch = searchList.any((v) => v == value);
+      if(!hasSearch) {
+        searchList.add(value);
+      }
     } catch(e) {
       List tempList = new List();
       tempList.add(value);
