b1bf01895bb23d1dcc5ff66d98bda830217fda80
dezhizhang <1541609448@qq.com>
2021-02-21 01:03:05 +0800

    实现视频播放

--- lib/pages/video/video.dart
+++ lib/pages/video/video.dart
@@ -1,6 +1,8 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:chewie/chewie.dart';
+import 'package:video_player/video_player.dart';
 import '../../utils/utils.dart';
 
 class Video extends StatelessWidget{
@@ -22,9 +24,38 @@ class Content extends StatefulWidget{
 
 class _Content extends State<Content>{
   @override
+  ChewieController chewieController;
+  VideoPlayerController videoPlayerController;
+  @override
+  void initState() { 
+    super.initState();
+    this.videoPlaye();
+  }
+  videoPlaye() {
+    videoPlayerController = VideoPlayerController.network(
+      'http://vfx.mtime.cn/Video/2019/02/04/mp4/190204084208765161.mp4'
+    );
+    chewieController = ChewieController(
+      videoPlayerController: videoPlayerController, 
+      aspectRatio: 3 / 2, 
+      autoPlay: true,
+      looping: true, 
+    );
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    videoPlayerController.dispose();
+    chewieController.dispose();
+  }
+  
   Widget build(BuildContext context) {
     ScreenAdapter.init(context);
     // TODO: implement build
-    return Container();
+    return Container(
+      child: Chewie(
+        controller: chewieController,
+      ),
+    );
   }
 }
