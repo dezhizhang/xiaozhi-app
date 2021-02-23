08e49c3e9faa664d9fbb22054db212b3d11933f4
dezhizhang <1541609448@qq.com>
2021-02-24 07:55:31 +0800

    进度条按钮

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-02-24 00:30:47
- * @LastEditTime: 2021-02-24 07:41:11
+ * @LastEditTime: 2021-02-24 07:54:31
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /xiaozhi/lib/main.dart
@@ -59,6 +59,7 @@ class MyApp extends StatelessWidget {
 
 
 
+
 class ButtonCase extends StatefulWidget {
   @override
   _ButtonCaseState createState() => _ButtonCaseState();
@@ -68,20 +69,31 @@ class _ButtonCaseState extends State<ButtonCase> {
   ButtonStatus _buttonStatus = ButtonStatus.none;
 
   _buildChild() {
-    if (_buttonStatus == ButtonStatus.none) {
+    // if (_buttonStatus == ButtonStatus.none) {
+    //   return Text(
+    //     '登 录',
+    //     style: TextStyle(color: Colors.white,fontSize: 18),
+    //   );
+    // } else if (_buttonStatus == ButtonStatus.loading) {
+    //   return CircularProgressIndicator(
+    //     backgroundColor: Colors.white,
+    //     strokeWidth: 2,
+    //   );
+    // } else if (_buttonStatus == ButtonStatus.done) {
+    //   return Icon(
+    //     Icons.check,
+    //     color: Colors.white,
+    //   );
+    // }
+    if(_buttonStatus == ButtonStatus.none) {
       return Text(
-        '登 录',
+        "登 录",
         style: TextStyle(color: Colors.white,fontSize: 18),
       );
-    } else if (_buttonStatus == ButtonStatus.loading) {
+    }else if(_buttonStatus == ButtonStatus.loading) {
       return CircularProgressIndicator(
         backgroundColor: Colors.white,
         strokeWidth: 2,
-      );
-    } else if (_buttonStatus == ButtonStatus.done) {
-      return Icon(
-        Icons.check,
-        color: Colors.white,
       );
     }
   }
@@ -92,17 +104,17 @@ class _ButtonCaseState extends State<ButtonCase> {
       color: Colors.blue,
       minWidth: 240,
       height: 48,
+      child: _buildChild(),
       onPressed: () {
         setState(() {
-          _buttonStatus = ButtonStatus.loading;
-          Future.delayed(Duration(seconds: 2), () {
-            setState(() {
-              _buttonStatus = ButtonStatus.done;
-            });
-          });
+           _buttonStatus = ButtonStatus.loading;
+           Future.delayed(Duration(seconds: 2),() {
+             setState(() {
+               _buttonStatus = ButtonStatus.done;
+             });
+           });
         });
       },
-      child: _buildChild(),
     );
   }
 }
