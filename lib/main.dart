3800365b6f99a3ce9fb4dcb089531d6b1c310998
dezhizhang <1541609448@qq.com>
2021-02-25 07:51:36 +0800

    提交

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-24 23:52:40
+ * @LastEditTime: 2021-02-25 07:51:12
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /xiaozhi/lib/main.dart
+ */
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -28,68 +36,10 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("AlertDialog"),
         ),
-        body: TabBarDemo(),
+        body: Text('hello'),
       ),
     );
   }
 }
 
-class TabBarDemo extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _TabBar();
-}
 
-class _TabBar extends State<TabBarDemo> with SingleTickerProviderStateMixin{
-  final List<String> _tabValues = [
-    '语文',
-    '英语',
-    '化学',
-    '物理',
-    '数学',
-    '生物',
-    '体育',
-    '经济',
-  ];
-
-  TabController? _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = TabController(
-      length: _tabValues.length,
-      vsync: ScrollableState(),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('TabBar'),
-        bottom: TabBar(
-          tabs: _tabValues.map((f) {
-            return Text(f);
-          }).toList(),
-          controller: _controller,
-          indicatorColor: Colors.red,
-          indicatorSize: TabBarIndicatorSize.tab,
-          isScrollable: true,
-          labelColor: Colors.red,
-          unselectedLabelColor: Colors.black,
-          indicatorWeight: 5.0,
-          labelStyle: TextStyle(height: 2),
-        ),
-      ),
-      body: TabBarView(
-        controller: _controller,
-        children: _tabValues.map((f) {
-          return Center(
-            child: Text(f),
-          );
-        }).toList(),
-      ),
-    );
-  }
-}
